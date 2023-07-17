84d107137fdd6545dc8508067711a41817c31dbb
Cong Nam <Congnam18165@gmail.com>
2023-07-17 21:56:52 +0700

    upload

--- main.js
+++ main.js
@@ -8,38 +8,24 @@ function render() {
         ul.innerHTML = "" // trong thẻ ul không có thẻ nào hết
     }
     else {
-        DataJobs.forEach((data) => { // duyệt qua các phần tử của mảng DataJobs
-            let li = document.createElement("li"); //tạo element li 
-            li.textContent = data;// cho thẻ li có nội dung là data
-            li.innerHTML += `<i class="trash fa-solid fa-trash-can"></i>`// thêm icon bên cạnh nội dung của thẻ li
-            li.addEventListener("click", function trash(event) {
-                event.target.parentElement.remove();
-                newjobs = DataJobs.filter((datajobs) => datajobs !== data)
-                // console.log(newjobs)
-                DataJobs = newjobs;
-                localStorage.setItem("Datas", JSON.stringify(newjobs))
-            })
-            ul.appendChild(li);// render thẻ li ra giao diện người dùng
-            li.classList.add("li")// thêm class li vào element li
-        });
+        getData(); //chạy hàm getData
     }
 }
 
 render(); //chạy hàm render
 function getData() {
-    // let Datas = JSON.parse(localStorage.getItem("Datas"))
     ul.innerHTML = ""; // Xóa nội dung hiện tại của ul
-    DataJobs.forEach((data) => { //duyệt qua các phần tử của mảng Datas
+    DataJobs.forEach((data) => { //duyệt qua các phần tử của mảng DataJobs
         let li = document.createElement("li"); // tạo element li 
         li.textContent = data; // cho thẻ li có nội dung là data
         li.innerHTML += `<i class="trash fa-solid fa-trash-can"></i>`// thêm icon bên cạnh nội dung của thẻ li
-        li.classList.add("li") // thêm class li vào element li
         li.addEventListener("click", function trash(event) {
-            event.target.parentElement.remove();
-            newjobs = DataJobs.filter((datajobs) => datajobs !== data)
-            DataJobs = newjobs;
-            localStorage.setItem("Datas", JSON.stringify(newjobs))
+            event.target.parentElement.remove(); // xóa thẻ element chứa icon thùng rác vừa click
+            newjobs = DataJobs.filter((datajobs) => datajobs !== data)// lọc mảng list công việc vừa tạo
+            DataJobs = newjobs;// gán mảng list hiện tại là mảng mới 
+            localStorage.setItem("Datas", JSON.stringify(newjobs))// lưu vào localStorage
         })
+        li.classList.add("li") // thêm class li vào element li
         ul.appendChild(li);// render thẻ li ra giao diện người dùng
 
     });
