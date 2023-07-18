ec141763042816fa671959944ce486c5ea735e10
Cong Nam <Congnam18165@gmail.com>
2023-07-19 00:26:42 +0700

    upload_day2

--- main.js
+++ main.js
@@ -1,80 +1,71 @@
 let button = document.querySelector(".button");
 let updateBtn = document.querySelector(".update")
 let input = document.querySelector(".input");
-let ul = document.querySelector(".ul");
-let test = document.querySelector(".test");
+let bodyTable = document.querySelector(".body-table");
+let pickDelete = document.querySelector(".delete-pick");
 
 let dataJobs = JSON.parse(localStorage.getItem("Datas")) !== null ? JSON.parse(localStorage.getItem("Datas")) : []; // lấy dữ liệu công việc trước đó ở localStorage
 function render() {
-    if (dataJobs === null) { // nếu mảng vừa lấy Datas có giá trị null thì thực hiện khối code dưới
-        table.innerHTML = "" // trong thẻ ul không có thẻ nào hết
-    }
-    else {
-        // table.innerHTML = `<tr>
-        // <th>STT</th><th>Tên Công Việc</th><th>Ngày thêm</th><th>Ngày sửa gần nhất</th>
-        // <th>Sửa</th><th>Xóa</th><th></th>
-        // </tr>`
-        getData(); //chạy hàm getData
+    
+    if (dataJobs.length > 0) {
+        let currentTime = JSON.parse(localStorage.getItem("currentTime"))
+        let currentNow = JSON.parse(localStorage.getItem("currentNow"))
+        getData(currentTime,currentNow); //chạy hàm getData
     }
 }
 
 render(); //chạy hàm render
-function getData() {
-    ul.innerHTML = ""; // Xóa nội dung hiện tại của ul
+function getData(currentTime,currentNow) {
+    bodyTable.innerHTML = "";
     dataJobs.forEach((data, index) => { //duyệt qua các phần tử của mảng DataJobs
-        let li = document.createElement("li"); // tạo element li 
-        
-        li.innerHTML = 
-        // `
-        // <tr>
-        // <td>${index+1}</td><td><span>${data}</span></td>
-        // <td></td><td></td>
-        // <td><button class="edit-btn"><i class="fa-solid fa-pen-to-square"></i></button></td>
-        // <td><i class="trash fa-solid fa-trash-can"></i></td>
-        // <td><input class="checkbox" type = "checkbox"></input></td>
-        // </tr>
-        
-        `<div class="box-jobs">
-        <input class="checkbox" type = "checkbox"></input>
-        <span>${data}</span>
-        <button class="edit-btn"><i class="fa-solid fa-pen-to-square"></i></button>
-        <i class="trash fa-solid fa-trash-can"></i>
-        </div>`
-        li.classList.add("li") // thêm class li vào element li
-        ul.appendChild(li);// render thẻ li ra giao diện người dùng
-        deleteItem(data, index);
-        edit(data, index);
-        deleteItems(index);
+        bodyTable.innerHTML +=
+            `
+        <tr>
+        <td>${index +1}</td><td><span>${data}</span></td>
+        <td>${currentTime  }</td><td>${currentNow == null ? "chưa sửa đổi lần nào": currentNow }</td>
+        <td><button class="edit-btn"><i class="fa-solid fa-pen-to-square"></i></button></td>
+        <td><i class="trash fa-solid fa-trash-can"></i></td>
+        <td><input class="checkbox" type = "checkbox"></input></td>
+        </tr>
+        `
     });
+    deleteItem();
+    edit();
+    deleteItems();
 }
-function deleteItem(data, index) {
-    let trash = document.querySelectorAll(".trash")[index]; // lấy DOM element có icon thùng rác
-    trash.addEventListener("click", function trashJob(event) {
-        event.target.parentElement.parentElement.remove(); // xóa thẻ element chứa icon thùng rác vừa click
-        newjobs = dataJobs.filter((dataJobs) => dataJobs !== data)// lọc mảng list công việc vừa tạo
-        dataJobs = newjobs;// gán mảng list hiện tại là mảng mới 
-        localStorage.setItem("Datas", JSON.stringify(newjobs))// lưu vào localStorage
-    })
+function deleteItem() {
+    let trash = document.getElementsByClassName("trash"); // lấy DOM element có icon thùng rác
+    for(let i = 0; i<trash.length; i++){
+        trash[i].addEventListener("click", function trashJob(event) {
+            event.target.parentElement.parentElement.remove(); // xóa thẻ element chứa icon thùng rác vừa click
+            newJobs = dataJobs.filter((dataJobs) => dataJobs !== event.target.parentElement.parentElement.querySelector("span").innerHTML)// lọc mảng list công việc vừa tạo
+            dataJobs = newJobs;// gán mảng list hiện tại là mảng mới 
+            localStorage.setItem("Datas", JSON.stringify(newJobs))// lưu vào localStorage
+        })
+    }
 }
-function edit(data, index) {
-    let editBtn = document.querySelectorAll(".edit-btn")[index];
-    editBtn.addEventListener("click", function editjob(e) {
-        input.value = e.target.parentElement.parentElement.querySelector("span").innerHTML
-        button.classList.add("none")
-        updateBtn.classList.remove("none")
-        let newJobs = dataJobs.filter((data) => data !== e.target.parentElement.parentElement.querySelector("span").innerHTML)
-        dataJobs = newJobs;
-        localStorage.setItem("Datas", JSON.stringify(newJobs))
-    })
+function edit() {
+    let editBtn = document.getElementsByClassName("edit-btn");
+    for(let i = 0; i<editBtn.length; i++){
+        editBtn[i].addEventListener("click", function editJob(e) {
+            input.value = e.target.parentElement.parentElement.parentElement.querySelector("span").innerHTML
+            button.classList.add("none")
+            updateBtn.classList.remove("none")
+            let newJobs = dataJobs.filter((data) => data !== e.target.parentElement.parentElement.parentElement.querySelector("span").innerHTML)
+            dataJobs = newJobs;
+            localStorage.setItem("Datas", JSON.stringify(newJobs))
+        })    
+    }
 }
-function deleteItems(index) {
-    test.addEventListener("click", function test() {
-        let isCheckbox = document.querySelectorAll(".checkbox")[index];
-        if(isCheckbox.checked == true ){
-           let newjobs = dataJobs.filter((data)=> data !== isCheckbox.parentElement.querySelector("span").innerHTML)
-           dataJobs = newjobs;
-           localStorage.setItem("Datas", JSON.stringify(newjobs))
-           location.reload();
+function deleteItems() {
+    pickDelete.addEventListener("click", function deleteJobs() {
+        let isCheckbox = document.getElementsByClassName("checkbox");
+        for(let i= 0; i<isCheckbox.length;i++)
+        if (isCheckbox[i].checked == true) {
+            let newJobs = dataJobs.filter((data) => data !== isCheckbox[i].parentElement.parentElement.querySelector("span").innerHTML)
+            dataJobs = newJobs;
+            localStorage.setItem("Datas", JSON.stringify(newJobs))
+            location.reload();
         }
     })
 }
@@ -82,7 +73,10 @@ function deleteItems(index) {
 updateBtn.addEventListener("click", function update() {
     dataJobs.push(input.value);
     localStorage.setItem("Datas", JSON.stringify(dataJobs))
-    getData(); // gọi hàm getData
+    let currentDate = new Date()
+    let currentNow = currentDate.getHours() +"/"+ currentDate.getMinutes() +"/"+ currentDate.getSeconds();
+    localStorage.setItem("currentTime",JSON.stringify(currentNow))
+    getData(currentNow); // gọi hàm getData
     input.value = "";
     button.classList.remove("none")
     updateBtn.classList.add("none")
@@ -98,9 +92,13 @@ button.onclick = function () {
             }
         })
         if (isDiffrent == true) {
+            let currentDate = new Date()
+            let currentTime = currentDate.getDay() +"/"+ currentDate.getMonth() + "/" + currentDate.getFullYear();
+            localStorage.setItem("currentTime",JSON.stringify(currentTime))
             dataJobs.push(input.value); // thêm giá trị ở ô input vào mảng DataJobs
             localStorage.setItem("Datas", JSON.stringify(dataJobs)) // lưu mảng DataJobs vào localStorage
-            getData(); // gọi hàm getData
+            // bodyTable.innerHTML = "";
+            getData(currentTime); // gọi hàm getData
             input.value = "";
         } else {
             alert("Việc làm đã bị trùng. Xin mời nhập lại")
